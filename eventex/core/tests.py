5807eb6d2e7205fdb1a3a7f830d99f7411791b9b
mabittar <ma_bittar@yahoo.com.br>
2021-02-17 15:46:30 -0300

    inscrições habilitadas

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 class HomeTest(TestCase):
     def setUp(self):
         self.response = self.client.get('/')
+
     def test_get(self):
         """GET / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
@@ -11,3 +12,6 @@ class HomeTest(TestCase):
     def testTemplate(self):
         """GET / must use index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
+
+    def test_subscription_link(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
